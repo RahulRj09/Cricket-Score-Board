7e1a266964b7703e74d80c2e0205a1f99dc001ff
RahulRj09 <rahul18@navgurukul.org>
2019-09-12 14:08:43 +0530

    Add. constructor in player class

--- src/main/java/scoreboard/Player.java
+++ src/main/java/scoreboard/Player.java
@@ -1,5 +1,13 @@
 package scoreboard;
 
 public class Player {
+    private String name;
+    private int score;
+    private int wickets;
 
+    public Player(String name, int score, int wickets) {
+        this.name = name;
+        this.score = score;
+        this.wickets = wickets;
+    }
 }
