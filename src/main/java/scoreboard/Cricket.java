574f96abcd620492fd1fad4c3ceabdc5ee9385ef
RahulRj09 <rahul18@navgurukul.org>
2019-09-12 14:15:36 +0530

    Add. initialize variables in Cricket class

--- src/main/java/scoreboard/Cricket.java
+++ src/main/java/scoreboard/Cricket.java
@@ -3,8 +3,11 @@ package scoreboard;
 import java.util.List;
 
 public class Cricket {
+    private List<Player> players;
+    private int over;
 
     public Cricket(List<Player> players, int over) {
-
+        this.players = players;
+        this.over = over;
     }
 }
