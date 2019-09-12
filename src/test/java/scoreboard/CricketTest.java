4383d90f3312a1d2a8b72c70153edaa069f5d834
RahulRj09 <rahul18@navgurukul.org>
2019-09-12 14:08:03 +0530

    Add. Player class for create a player

--- src/test/java/scoreboard/CricketTest.java
+++ src/test/java/scoreboard/CricketTest.java
@@ -2,11 +2,14 @@ package scoreboard;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CricketTest {
     @Test
     public void shouldBePlay() {
-
+        List<Player> players = new ArrayList<>();
     }
 }
