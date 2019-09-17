a372e036af800961a87a9e07b96eced80761e964
RahulRj09 <rahul18@navgurukul.org>
2019-09-17 15:01:09 +0530

    Add. getBowlers method in cricket class

--- src/main/java/scoreboard/Cricket.java
+++ src/main/java/scoreboard/Cricket.java
@@ -4,25 +4,27 @@ import java.util.List;
 import java.util.Random;
 
 public class Cricket {
-    private List<Player> players;
+    private final List<Player> bowlers;
+    private List<Player> batsman;
     private int over;
     private int currentPlayerIndex;
     private int currentBowlerIndex;
 
     public Cricket(List<Player> batsman, List<Player> bowlers, int over) {
-        this.players = batsman;
+        this.batsman = batsman;
         this.over = over;
+        this.bowlers = bowlers;
         this.currentPlayerIndex = 0;
         this.currentBowlerIndex = bowlers.size() - 1;
     }
 
     public void play() {
         for (int i = 1; i <= over * 6; i++) {
-            Player currentPlayer = players.get(currentPlayerIndex);
+            Player currentPlayer = batsman.get(currentPlayerIndex);
             int run = getRun();
             currentPlayer.setBatsmanRuns(run);
             currentPlayer.setBatsmanTotalPlayedBalls();
-            Player bowler = players.get(currentBowlerIndex);
+            Player bowler = bowlers.get(currentBowlerIndex);
             bowler.setBall();
             if (run == 5) {
                 bowler.setWickets(1);
@@ -32,7 +34,7 @@ public class Cricket {
             if (i % 6 == 0) {
                 this.currentBowlerIndex -= 1;
             }
-            Board board = new Board(players, over);
+            Board board = new Board(batsman, over);
             board.print();
             currentPlayerIndex = run % 2;
         }
@@ -43,10 +45,14 @@ public class Cricket {
     }
 
     public List<Player> getPlayers() {
-        return players;
+        return batsman;
     }
 
     private int getRun() {
         return new Random().nextInt(6) + 1;
     }
+    public List<Player> getBowlers() {
+        return bowlers;
+    }
+
 }
