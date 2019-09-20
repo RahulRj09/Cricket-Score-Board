ac4966a08fc1e3ab353cf554328bd8d34061973e
RahulRj09 <rahul18@navgurukul.org>
2019-09-20 13:29:23 +0530

    Add. getter method in Team class

--- src/main/java/scoreboard/Team.java
+++ src/main/java/scoreboard/Team.java
@@ -3,10 +3,18 @@ package scoreboard;
 import java.util.List;
 
 public class Team {
-    List<Player> players;
-    String name;
+    private List<Player> players;
+    private String name;
     public Team(List<Player> players, String name) {
         this.players = players;
         this.name = name;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public String getName() {
+        return name;
     }
 }
